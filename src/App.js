ce64e19a0b4d3c5f20e09adc2ff63e6987c83b2d
Michał Kopczewski <kopczewski.michal101@gmail.com>
2024-04-24 22:01:41 +0200

    Ustawiono czcionkę na Piotrową

--- src/App.js
+++ src/App.js
@@ -1,41 +1,33 @@
-import './App.css';
+import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import {React, useEffect} from "react"
+import { React, useEffect } from "react";
 
-import MainPage from './components/MainPage/MainPage';
-import NoteMaster from './components/MainPage/Projects/trener-nut';
-import Neunet1 from './components/MainPage/Projects/Neunet1';
-import GraKarciana from './components/MainPage/Projects/GraKarciana';
+import Layout from "./Layout";
+import MainPage from "./components/MainPage/MainPage";
+import NoteMaster from "./components/MainPage/Projects/trener-nut";
+import Neunet1 from "./components/MainPage/Projects/Neunet1";
+import GraKarciana from "./components/MainPage/Projects/GraKarciana";
 
 function App() {
   useEffect(() => {
-    document.title = 'UG NaN++';
+    document.title = "UG NaN++";
   }, []);
   return (
-    <div className="App">
-      <BrowserRouter>
-        <Routes>
-          <Route
-            path="/"
-            element={<MainPage/>}
-          ></Route>
-          <Route
-            path="/projects/trener-nut"
-            element={<NoteMaster/>}
-          ></Route>
-          <Route
-            path="/projects/neunet1"
-            element={<Neunet1/>}
-          ></Route>
-          <Route
-            path="/projects/gra_karciana"
-            element={<GraKarciana/>}
-          ></Route>
-
-          
-        </Routes>
-      </BrowserRouter>
-    </div>
+    <Layout>
+      <div className="App">
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<MainPage />}></Route>
+            <Route path="/projects/trener-nut" element={<NoteMaster />}></Route>
+            <Route path="/projects/neunet1" element={<Neunet1 />}></Route>
+            <Route
+              path="/projects/gra_karciana"
+              element={<GraKarciana />}
+            ></Route>
+          </Routes>
+        </BrowserRouter>
+      </div>
+    </Layout>
   );
 }
 
---
+++ src/Layout.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Helmet } from 'react-helmet';
+
+const Layout = ({ children }) => (
+  <>
+    <Helmet>
+      <link rel="preconnect" href="https://fonts.googleapis.com" />
+      <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+      <link
+        href="https://fonts.googleapis.com/css2?family=Chakra+Petch:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&display=swap"
+        rel="stylesheet"
+      />
+    </Helmet>
+    {children}
+  </>
+);
+
+export default Layout;
